28ef76404ebb086d84ec76d093e7c17b9fb6e5a4
더올림 <theollim04@gmail.com>
2026-08-29 15:54:26 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+/*
+  Supabase 프로젝트를 만든 뒤 아래 두 값만 입력하세요.
+  service_role 키와 계정 비밀번호는 절대 넣지 않습니다.
+*/
+window.DEOOLLIM_CONFIG = {
+  supabaseUrl: "https://dssqocptxfrfzpdycbvp.supabase.co",
+  supabasePublishableKey: "sb_publishable_TycMj99uY0suB0NU2FJrWg_xeDV_BMP"
+};
